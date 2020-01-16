2b7c64df800dd76f0682fa97218c6de5e1d76de3
davidtissera1 <davidtissera@outlook.com>
2020-01-16 15:19:49 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'react/jsx-indent': [2, 4],
+    'indent': ['error', 4],
+    'jsx-quotes': [2, 'prefer-single'],
+    'react/jsx-curly-spacing': [2, 'always'],
+  },
+};
